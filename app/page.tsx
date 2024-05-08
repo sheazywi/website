8980a56944e741ac2e02fde3e71803d9d8f32964
sheazy_wi <73042839+sheazywi@users.noreply.github.com>
2024-05-08 00:00:56 +0000

    Pending changes exported from your codespace

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
-import HeroSection from '@/components/sections/homepage/HeroSection'
+import React from 'react';
+import HeroSection from '@/components/sections/homepage/HeroSection';
+import AchievementsSection from '@/components/sections/homepage/AchievementSection';
+import AboutSection from '@/components/sections/homepage/AboutSection';
+import ProjectsSection from '@/components/sections/homepage/ProjectsSection';
+import EmailSection from '@/components/sections/homepage/EmailSection';
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col bg-[#121212] countainer nx-auto px-12 py-4">
-    <HeroSection />
-  </main>
+  return 
+  <main className="flex min-h-screen flex-col bg-[#121212]">
+    <div className="container mt-24 mx-auto px-12 py-4">
+      <HeroSection />
+      <AchievementsSection />
+      <AboutSection />
+      <ProjectsSection />
+      <EmailSection />
+    </div>
+</main>
 }
